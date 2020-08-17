a9acd9ac358fea4ff52ffd36de0855cb91bbd3c0
songyu3044 <2634039766@qq.com>
2020-08-17 20:29:45 +0800

    初始化

--- src/views/function/heatMap.vue
+++ src/views/function/heatMap.vue
@@ -1,8 +1,16 @@
 <template>
-
+<basicmap></basicmap>
 </template>
 <script>
+  import basicmap from '../map/basicMap'
 export default {
+data(){
 
+},
+methods:{
+},
+ components: {
+      basicmap: basicmap,
+    },
 }
 </script>
