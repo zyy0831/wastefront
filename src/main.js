3425ec237726c66537e132c1bbae99b4ea0bde22
zyy0831 <798206656@qq.com>
2020-08-18 18:20:38 +0800

    新增 axios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import qs from 'qs'
 import store from './store/city'
 // 使用ElementUI
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios    //全局注册，使用方法为:this.$axios
+Vue.prototype.qs = qs           //全局注册，使用方法为:this.qs
 
 new Vue({
   router,
