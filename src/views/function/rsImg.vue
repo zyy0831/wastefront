11adf04002f579c4e83d5ed11ee029efde519f0d
1339630594 <1339630594@qq.com>
2020-08-18 09:07:13 +0800

    遥感图像

--- src/views/function/rsImg.vue
+++ src/views/function/rsImg.vue
@@ -1,22 +1,82 @@
 <template>
-  <basicmap></basicmap>
+  <div id="shpDiv">
+    <basicmap ref="map"></basicmap>
+    <el-form class="air" :label-position="labelPosition">
+      <el-form-item label="切换遥感影像">
+      </el-form-item>
+      <el-form-item label="01">
+         <el-checkbox v-model="checked1" @change="show()">tiff1</el-checkbox>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
 <script>
-  import basicmap from '../map/basicMap'
-  export default {
-    data() {
-      return {
-
-      }
+import "ol/ol.css";
+import { Tile as TileLayer } from "ol/layer";
+import { TileWMS } from "ol/source";
+import basicmap from "../map/basicMap";
+export default {
+  data() {
+    return {
+      labelPosition:'right',
+      map: null,
+      wmsLayer: null,
+      URL1: "http://10.100.18.67:8080/geoserver/cite/wms",
+      checked1: true,
+    };
+  },
+  mounted() {
+    this.initMap();
+    this.getWMS();
+  },
+  methods: {
+    initMap() {
+      this.map = this.$store.state.map;
     },
-    components: {
-      basicmap: basicmap,
+    getWMS() {
+      (this.wmsLayer = new TileLayer({
+        visible: this.checked1,
+        source: new TileWMS({
+          params: {
+            FORMAT: "image/png",
+            VERSION: "1.1.0",
+            LAYERS: "	cite:0816",
+            TILED: true,
+            STYLES: "",
+            transparent: true,
+            opacity: 1,
+          },
+          url: this.URL1,
+          serverType: "geoserver",
+        }),
+      })),
+        this.map.addLayer(this.wmsLayer);
     },
-  }
-
+    show() {
+      this.checked1 = !this.checked1;
+      this.wmsLayer.setVisible(this.checked1);
+    },
+  },
+  components: {
+    basicmap: basicmap,
+  },
+};
 </script>
+
 <style scoped>
-  #infoCss {
-    height: 100%;
-    width: 100%;
-  }
+#shpDiv {
+  width: 100%;
+  height: 100%;
+}
+.air {
+  z-index: 999;
+  width: 15%;
+  top: 10%;
+  right: 3%;
+  position: absolute;
+  /* background-color: rgb(25, 202, 54); */
+  opacity: 0.9;
+  background-color: white;
+}
+</style>
--- src/views/function/shp.vue
+++ src/views/function/shp.vue
@@ -1,63 +1,22 @@
 <template>
-  <basicmap ref="map"></basicmap>
+  <basicmap></basicmap>
 </template>
-
 <script>
-import "ol/ol.css";
-import {
-  Tile as TileLayer,
-} from "ol/layer";
-import{TileWMS} from "ol/source"
+  import basicmap from '../map/basicMap'
+  export default {
+    data() {
+      return {
 
+      }
+    },
+    components: {
+      basicmap: basicmap,
+    },
+  }
 
-import basicmap from "../map/basicMap";
-export default {
-  data() {
-    return {
-      map: null,
-      wmsLayer: null,
-    };
-  },
-  mounted() {
-    this.initMap();
-    this.getWMS();
-  },
-  methods: {
-    initMap() {
-      this.map = this.$store.state.map;
-    },
-    getWMS() {
-      this.wmsLayer = new TileLayer({
-        
-        source: new TileWMS({
-          params: {
-            FORMAT: "image/png",
-            VERSION: "1.1.0",
-            LAYERS: "	karl:Export_Output1",
-            TILED: true,
-            STYLES: "",
-            transparent: true,
-            opacity:1,
-          },
-          url: "http://localhost:8080/geoserver/karl/wms?service=WMS",
-             serverType:'geoserver'
-         
-        }),
-      }),
-      this.map.addLayer(this.wmsLayer);
-      console.log(this.map)
-    },
-  },
-  components: {
-    basicmap: basicmap,
-  },
-};
 </script>
-
 <style scoped>
-#map {
-  width: 1000px;
-  height: 700px;
-  border: 1px solid #ff0000;
-}
-</style>
+  #infoCss {
+    height: 100%;
+    width: 100%;
+  }
