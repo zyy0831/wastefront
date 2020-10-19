5fefdb0c2db63f5288e59f15946b4431f36c8909
yangheng2020 <1258062009@qq.com>
2020-10-19 15:16:36 +0800

    遥感影像添加按钮

--- src/views/function/rsImg.vue
+++ src/views/function/rsImg.vue
@@ -2,8 +2,17 @@
   <div id="shpDiv">
     <basicmap ref="map"></basicmap>
     <el-card class="airSel">
-      <el-select v-model="filterPropName" placeholder="请选择" @change="Query(filterPropName)">
-        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+      <el-select
+        v-model="filterPropName"
+        placeholder="请选择"
+        @change="Query(filterPropName)"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
         </el-option>
       </el-select>
     </el-card>
@@ -13,29 +22,45 @@
         <el-checkbox v-model="checked1" @change="show()">tiff1</el-checkbox>
       </el-form-item>
     </el-form>
-    <el-form class="airLeft" :label-position="labelPosition" :model="form">
-      <el-form-item label="地址">
-        <el-input v-model="form.Address"></el-input>
-      </el-form-item>
-      <el-form-item label="面积">
-        <el-input v-model="form.Area"></el-input>
-      </el-form-item>
-      <el-form-item label="个数">
-        <el-input v-model="form.Id"></el-input>
-      </el-form-item>
-      <el-form-item label="编号">
-        <el-input v-model="form.Num"></el-input>
-      </el-form-item>
-      <el-form-item label="类型">
-        <el-input v-model="form.Type"></el-input>
-      </el-form-item>
-      <el-form-item label="体积">
-        <el-input v-model="form.Vol"></el-input>
-      </el-form-item>
-      <el-form-item label="备注">
-        <el-input v-model="form.Remake"></el-input>
-      </el-form-item>
-    </el-form>
+    <div>
+      <el-form
+        class="airLeft"
+        :label-position="labelPosition"
+        :model="form"
+        size="mini"
+        v-show="showfrom"
+      >
+        <el-form-item style="background-color:white;">
+          <el-button type="primary"
+            @click="closeform()"
+            size="mini"
+            icon="el-icon-close"
+            style="float: right; z-index: 999;background-color:red ;border-color :red!important; font-size:5px" >
+          </el-button>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input v-model="form.Address"></el-input>
+        </el-form-item>
+        <el-form-item label="面积">
+          <el-input v-model="form.Area"></el-input>
+        </el-form-item>
+        <el-form-item label="个数">
+          <el-input v-model="form.Id"></el-input>
+        </el-form-item>
+        <el-form-item label="编号">
+          <el-input v-model="form.Num"></el-input>
+        </el-form-item>
+        <el-form-item label="类型">
+          <el-input v-model="form.Type"></el-input>
+        </el-form-item>
+        <el-form-item label="体积">
+          <el-input v-model="form.Vol"></el-input>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.Remake"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -51,22 +76,23 @@ import { Style, Stroke, Fill, Icon } from "ol/style";
 import { Point, Polygon, MultiPolygon } from "ol/geom";
 import { circular } from "ol/geom/Polygon";
 import Overlay from "ol/Overlay";
-import {GeoJSON,WFS} from 'ol/format';
-import {equalTo} from 'ol/format/filter';
-import {bbox} from 'ol/loadingstrategy';
+import { GeoJSON, WFS } from "ol/format";
+import { equalTo } from "ol/format/filter";
+import { bbox } from "ol/loadingstrategy";
 export default {
   data() {
     return {
       labelPosition: "right",
+      showfrom: true,
       map: null,
       layer_wms_tiff: null,
       layer_wms_shp: null,
       wfsVectorLayer: null,
-        vector: null,
-        vectorSource: null,
+      vector: null,
+      vectorSource: null,
       url_geoser: "http://localhost:8080/geoserver/wastr/wms?service=WMS",
       checked1: true,
-        featuresGeo: null,
+      featuresGeo: null,
       form: {
         Address: "", //地址
         Area: "", //面积
@@ -76,35 +102,44 @@ export default {
         Type: "", //类型
         Vol: "", //体积
       },
-        options: [{
-          value: '工程渣土',
-          label: '工程渣土'
-        }, {
-          value: '工程泥浆',
-          label: '工程泥浆'
-        }, {
-          value: '拆房垃圾',
-          label: '拆房垃圾'
-        }, {
-          value: '工程垃圾',
-          label: '工程垃圾'
-        }, {
-          value: '装修垃圾',
-          label: '装修垃圾'
-        }
-        ],
-        filterPropName: ''
+      options: [
+        {
+          value: "工程渣土",
+          label: "工程渣土",
+        },
+        {
+          value: "工程泥浆",
+          label: "工程泥浆",
+        },
+        {
+          value: "拆房垃圾",
+          label: "拆房垃圾",
+        },
+        {
+          value: "工程垃圾",
+          label: "工程垃圾",
+        },
+        {
+          value: "装修垃圾",
+          label: "装修垃圾",
+        },
+      ],
+      filterPropName: "",
     };
   },
   mounted() {
     this.initMap();
     // this.getWMS_tiff("	cite:pingdingshan201904031");
-     this.getWMS_tiff("		wastr:pingdingshan201904031");
+    this.getWMS_tiff("		wastr:pingdingshan201904031");
     // this.getWMS_tiff();
     this.getWMS_shp();
     // this.getWFS();
+    // this.closeform();
   },
   methods: {
+    closeform() {
+      this.showfrom = false;
+    },
     getWMS_shp() {
       (this.layer_wms_shp = new TileLayer({
         visible: this.checked1,
@@ -123,30 +158,31 @@ export default {
         }),
       })),
         this.map.addLayer(this.layer_wms_shp);
-      },
-      getWFS() {
-        //创建wfs资源
-        let wfsVectorSource = new VectorSource({
-          format: new GeoJSON(),
-          projection: 'EPSG:4326',
-          url: 'http://localhost:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite%3AExport_Output&maxFeatures=50&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2',
-          strategy: bbox
-        });
-        // console.log(wfsVectorSource);
-        //创建wfs图层，注意需要设置好描边样式，否则不展示效果出来
-        this.wfsVectorLayer = new VectorLayer({
-          source: wfsVectorSource,
-          style: new Style({
-            stroke: new Stroke({
-              color: 'blue',
-              width: 2
-            })
+    },
+    getWFS() {
+      //创建wfs资源
+      let wfsVectorSource = new VectorSource({
+        format: new GeoJSON(),
+        projection: "EPSG:4326",
+        url:
+          "http://localhost:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite%3AExport_Output&maxFeatures=50&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2",
+        strategy: bbox,
+      });
+      // console.log(wfsVectorSource);
+      //创建wfs图层，注意需要设置好描边样式，否则不展示效果出来
+      this.wfsVectorLayer = new VectorLayer({
+        source: wfsVectorSource,
+        style: new Style({
+          stroke: new Stroke({
+            color: "blue",
+            width: 2,
           }),
-          visible: true
-        });
-        // console.log(this.wfsVectorLayer);
-        this.map.addLayer(this.wfsVectorLayer);
-      },
+        }),
+        visible: true,
+      });
+      // console.log(this.wfsVectorLayer);
+      this.map.addLayer(this.wfsVectorLayer);
+    },
     initMap() {
       this.map = this.$store.state.map;
       this.map.on("click", this.mapClick);
@@ -176,63 +212,65 @@ export default {
     },
     mapClick: function (evt) {
       let _that = this;
-        let viewResolution = _that.map.getView().getResolution();
-        let url_wms_Shp = _that.layer_wms_shp
-          .getSource()
-          .getFeatureInfoUrl(evt.coordinate, viewResolution, "EPSG:4326", {
-            INFO_FORMAT: "application/json",
-          });
-        // console.log(_that.layer_wms_tiff.get('name'))
-        // console.log(_that.map.getLayers().array_)
-        this.$axios({
-            method: "get",
-            url: url_wms_Shp,
-          })
-          .then((res) => {
-            // console.log(res.data.features[0].properties);
-            this.form = res.data.features[0].properties;
-            // this.name = res.data.features[0].properties.name;
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      },
-      //属性查询
-      Query(val) {
-        if(this.featuresGeo != null){
-          this.vector.getSource().clear(); //清楚图层中的要素
-        }
-        this.vectorSource = new VectorSource();
-        this.vector = new VectorLayer({
-          source: this.vectorSource,
-          style: new Style({
-            stroke: new Stroke({
-              color: 'rgba(0, 0, 255, 1.0)',
-              width: 5,
-            }),
+      let viewResolution = _that.map.getView().getResolution();
+      let url_wms_Shp = _that.layer_wms_shp
+        .getSource()
+        .getFeatureInfoUrl(evt.coordinate, viewResolution, "EPSG:4326", {
+          INFO_FORMAT: "application/json",
+        });
+      // console.log(_that.layer_wms_tiff.get('name'))
+      // console.log(_that.map.getLayers().array_)
+      this.$axios({
+        method: "get",
+        url: url_wms_Shp,
+      })
+        .then((res) => {
+          // console.log(res.data.features[0].properties);
+          this.form = res.data.features[0].properties;
+          // this.name = res.data.features[0].properties.name;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    //属性查询
+    Query(val) {
+      if (this.featuresGeo != null) {
+        this.vector.getSource().clear(); //清楚图层中的要素
+      }
+      this.vectorSource = new VectorSource();
+      this.vector = new VectorLayer({
+        source: this.vectorSource,
+        style: new Style({
+          stroke: new Stroke({
+            color: "rgba(0, 0, 255, 1.0)",
+            width: 5,
           }),
-        });
-        // 创建一个请求
-        let featureRequest = new WFS().writeGetFeature({
-          srsName: 'EPSG:4326',
-          featureNS: 'http://www.openplans.org/wastr', //修改为工作区URL
-          featurePrefix: 'wastr', //名字
-          featureTypes: ['wastr:Export_Output'], //名字
-          outputFormat: 'application/json',
-          filter: equalTo('Type', val)
-        });
-        // 发送请求
-        fetch('http://10.100.18.67:8080/geoserver/wfs', {
-          method: 'POST',
-          body: new XMLSerializer().serializeToString(featureRequest),
-        }).then(res => {
+        }),
+      });
+      // 创建一个请求
+      let featureRequest = new WFS().writeGetFeature({
+        srsName: "EPSG:4326",
+        featureNS: "http://www.openplans.org/wastr", //修改为工作区URL
+        featurePrefix: "wastr", //名字
+        featureTypes: ["wastr:Export_Output"], //名字
+        outputFormat: "application/json",
+        filter: equalTo("Type", val),
+      });
+      // 发送请求
+      fetch("http://10.100.18.67:8080/geoserver/wfs", {
+        method: "POST",
+        body: new XMLSerializer().serializeToString(featureRequest),
+      })
+        .then((res) => {
           return res.json();
-        }).then(json => {
+        })
+        .then((json) => {
           this.featuresGeo = new GeoJSON().readFeatures(json);
           this.vectorSource.addFeatures(this.featuresGeo);
         });
-        this.map.addLayer(this.vector)
-      }
+      this.map.addLayer(this.vector);
+    },
   },
   components: {
     basicmap: basicmap,
@@ -246,51 +284,47 @@ export default {
   height: 100%;
 }
 .airRight {
-  z-index: 999;
+  z-index: 888;
   width: 15%;
   top: 12%;
   right: 3%;
   position: absolute;
   /* background-color: rgb(25, 202, 54); */
   opacity: 0.9;
-  background-color:paleturquoise;
-  border-style:solid;
-  border-width:10px;
-    border-color:rebeccapurple;
+  background-color: paleturquoise;
+  border-style: solid;
+  border-width: 5px;
+  border-color: rebeccapurple;
   border-bottom-color: transparent;
-
 }
 
-  .airSel {
-    z-index: 999;
-    width: 15%;
-    top: 27%;
-    right: 3%;
-    position: absolute;
-    /* background-color: rgb(25, 202, 54); */
-    opacity: 0.9;
-    background-color:paleturquoise;
-    border-style:solid;
-    border-width:10px;
-   border-color:rebeccapurple;
-
-  }
+.airSel {
+  z-index: 888;
+  width: 15%;
+  top: 26.3%;
+  right: 3%;
+  position: absolute;
+  /* background-color: rgb(25, 202, 54); */
+  opacity: 0.9;
+  background-color: paleturquoise;
+  border-style: solid;
+  border-width: 5px;
+  border-color: rebeccapurple;
+}
 
 .airLeft {
-  z-index: 999;
+  z-index: 888;
   width: 20%;
-  height: 80%;
+  height: 59.1%;
   top: 9%;
-  right: 62%;
+  right: 61.6%;
   position: absolute;
   /* background-color: rgb(25, 202, 54); */
   opacity: 0.7;
-  background-color:paleturquoise;
-  border-style:solid;
-  border-width:10px;
-  border-color:rebeccapurple;
-font:red;
-
+  background-color: paleturquoise;
+  border-style: solid;
+  border-width: 5px;
+  border-color: rebeccapurple;
+  font: red;
 }
-
 </style>
