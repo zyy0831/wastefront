cdab6adf002fb781da39c710919551f1338d6d44
jiajj2020 <767425723@qq.com>
2020-10-20 20:53:29 +0800

    监管单位

---
+++ src/views/infoSearch/monitUnit.vue
@@ -0,0 +1,38 @@
+ <template>
+  <el-table :data="jgTable">
+    <el-table-column prop="member" label="成员"></el-table-column>
+    <el-table-column prop="tele" label="电话"></el-table-column>
+    <el-table-column prop="Company" label="单位"></el-table-column>
+    <el-table-column prop="post" label="负责人"></el-table-column>
+  </el-table>
+</template>
+<script>
+import * as getCompanyAPI from "../../api/company";
+export default {
+  data() {
+    return {
+      jgTable: [{
+        member: "",
+        tele: "",
+        Company: "",
+        post: "",
+      }],
+    };
+  },
+  methods: {
+    getCompany01() {
+      // 获取监管单位数据
+      getCompanyAPI.getCompany().then((res) => {
+          console.log(res);
+          this.jgTable = res.data;
+        }).catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    this.getCompany01();
+    // jgTable = api.data
+  },
+};
+</script>
