37ddf0c06157c9a288ffd1c92dc9d0f9ebaadab8
jiajj2020 <767425723@qq.com>
2020-10-20 21:34:27 +0800

    监管数据api

--- src/api/company.js
+++ src/api/company.js
@@ -6,3 +6,24 @@ import http from 'axios';
 export function getCompany(){
   return http.get('company/getcompany');
 }
+
+/**
+ * 查询某一个设施信息
+ */
+export function getAnnualdata(){
+    return http.get('annualdata/getAnnualdata');
+  }
+
+  /**
+ * 查询某一个设施信息
+ */
+export function getlawyer(){
+    return http.get('lawyers/getlawyer');
+  }
+
+  /**
+ * 查询某一个设施信息
+ */
+export function getStock(){
+    return http.get('stock/getStock');
+  }
