63523bb829f41c26e103e4a0f30bc52c54f6b0be
yangheng2020 <1258062009@qq.com>
2020-10-21 09:23:17 +0800

    222

---
+++ src/views/function/three.vue
@@ -0,0 +1,9 @@
+<template>
+<iframe src="http://10.110.178.122:8080/cesiumMap/map.html" class="guangPu"></iframe>
+</template>
+<style scoped>
+    .guangPu{
+        width: 100%;
+        height: 100%;
+    }
+</style>
